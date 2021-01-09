6104e775412cccc9b878d0ae966ba0dab8037b74
Fumihiro (Ben) Bessho <iwabako@gmail.com>
2021-01-09 12:35:56 -0500

    Added user Tweet timeline endpoint

--- TwitterAPI/constants.py
+++ TwitterAPI/constants.py
@@ -245,6 +245,7 @@ ENDPOINTS = {
     'tweets/search/stream/rules':                           ('POST',   'api'), # use method_override for 'GET'
     'users':                                                ('GET',    'api'),
     'users/:PARAM':                                         ('GET',    'api'), # ID
+    'users/:PARAM/tweets':                                  ('GET',    'api'), # ID
     'users/by':                                             ('GET',    'api'),
     'users/by/username/:PARAM':                             ('GET',    'api'), # USERNAME
 }
